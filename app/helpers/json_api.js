ac19e99d7fabf330ffee7f8b0f56bd920cedb13b
Juan Carlos Da Silva <jcda_23@hotmail.com>
2022-08-01 02:34:40 -0500

    primer segundo commit

--- app/helpers/json_api.js
+++ app/helpers/json_api.js
@@ -1,6 +1,6 @@
 const NAME = "css-tricks",
-  DOMAIN = "./app/api.json",
-  SLIDE = "./app/slider.json";
+  DOMAIN = "api.json",
+  SLIDE = "slider.json";
 /*   DOMAIN = "https://css-tricks.com",
   SITE = "https://css-tricks.com/wp-json",
   API_WP = "https://css-tricks.com/wp-json/wp/v2",
